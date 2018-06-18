9367927471f363c9efe3376bf115b07173eb218b
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-06-18 15:39:49 -0700

    Displaying library, ahem, movie collection

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
 
 class Library extends Component {
+  constructor(){
+    super();
 
+    this.state = {
+      movies: []
+    };
+
+  }
+
+  componentDidMount(){
+
+    axios.get('http://localhost:3001/movies')
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        movies: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 
   render() {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie movieData={movie} key={index} />
+    })
+    console.log(movies);
     return (
       <div>
-      COLLECTION OF MOVIES.... CALLED LIBRARY
+        {movies}
       </div>
     );
   }
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 
 class Movie extends Component {
   static propTypes = {
-		id: PropTypes.number,
-    title: PropTypes.string,
-    overview: PropTypes.string,
-    release_date: PropTypes.string,
-		image_url: PropTypes.string,
-    inventory: PropTypes.number
+    movieData: PropTypes.object
+		// id: PropTypes.number,
+    // title: PropTypes.string,
+    // overview: PropTypes.string,
+    // release_date: PropTypes.string,
+		// image_url: PropTypes.string,
+    // inventory: PropTypes.number
   }
 
   render() {
     return (
       <div>
-        <h2>{this.props.title}</h2>
-        <h3>{this.props.overview}</h3>
-        <h3>{this.props.release_date}</h3>
-        <h3>{this.props.inventory}</h3>
+        <h2>{this.props.movieData.title}</h2>
+        <img src={this.props.movieData.image_url} alt="movie_poster"/>
+        <h3>{this.props.movieData.overview}</h3>
+        <h3>{this.props.movieData.release_date}</h3>
+        <h3>{this.props.movieData.inventory}</h3>
       </div>
     );
   }
