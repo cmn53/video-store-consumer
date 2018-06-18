bca82bc30d419edc68c10d1b72a92cb17a1ce1dc
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-06-18 14:22:49 -0700

    Created movie component

---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Movie extends Component {
+  static PropTypes = {
+    title: PropTypes.string,
+    overview: PropTypes.string,
+    release_date: PropTypes.object,
+    inventory: PropTypes.number
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>this.props.title</h2>
+        <h3>this.props.overview</h3>
+        <h3>this.props.release_date</h3>
+        <h3>this.props.inventory</h3>
+      </div>
+    );
+  }
+}
+
+export default Movie;
