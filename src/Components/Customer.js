abbba5567eff9bb675f2d7b907d2eb73a746e60b
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-06-18 15:33:27 -0700

    Displaying customer collection

--- src/Components/Customer.js
+++ src/Components/Customer.js
@@ -3,25 +3,26 @@ import PropTypes from 'prop-types';
 
 class Customer extends Component {
   static propTypes = {
-		id: PropTypes.number,
-    name: PropTypes.string,
-    registered_at: PropTypes.object,
-    address: PropTypes.string,
-    city: PropTypes.string,
-    state: PropTypes.string,
-    postal_code: PropTypes.string,
-    phone: PropTypes.string,
-    account_credit: PropTypes.number
+    customerData: PropTypes.object
+		// id: PropTypes.number,
+    // name: PropTypes.string,
+    // registered_at: PropTypes.object,
+    // address: PropTypes.string,
+    // city: PropTypes.string,
+    // state: PropTypes.string,
+    // postal_code: PropTypes.string,
+    // phone: PropTypes.string,
+    // account_credit: PropTypes.number
   }
 
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <h3>Registered at: {this.props.registered_at}</h3>
-        <h3>Address: {this.props.address} {this.props.city}, {this.props.state} {this.props.postal_code}</h3>
-        <h3>Phone: {this.props.phone}</h3>
-        <h3>Account credit: {this.props.account_credit}</h3>
+        <h2>{this.props.customerData.name}</h2>
+        <h3>Registered at: {this.props.customerData.registered_at}</h3>
+        <h3>Address: {this.props.customerData.address} {this.props.customerData.city}, {this.props.customerData.state} {this.props.customerData.postal_code}</h3>
+        <h3>Phone: {this.props.customerData.phone}</h3>
+        <h3>Account credit: {this.props.customerData.account_credit}</h3>
       </div>
     );
   }
--- src/Components/Customers.js
+++ src/Components/Customers.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Customer from './Customer';
 
 class Customers extends Component {
+  constructor(){
+		super();
 
+		this.state = {
+			customers: []
+		};
+
+	}
+
+	componentDidMount(){
+
+		axios.get('http://localhost:3001/customers')
+		.then((response) => {
+			console.log(response.data);
+			this.setState({
+				customers: response.data
+			})
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}
 
   render() {
+    const customers = this.state.customers.map((customer, index) => {
+      return <Customer customerData={customer} key={index} />
+    })
+    console.log(customers);
     return (
       <div>
-      COLLECTION OF CUSTOMERS
+        {customers}
       </div>
     );
   }
